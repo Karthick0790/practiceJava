01edae2b70d6fff7a6329f9b3900e73008d10220
Karthick Kumar <karthick@bluejeansnet.com>
2016-08-13 16:12:13 +0530

    Adjusted spacings

--- Factorial.java
+++ Factorial.java
@@ -2,9 +2,9 @@ public class Factorial
 {
   public static void main (String[] args)
   {
-     if(args.length > 0)
+	 if(args.length > 0)
 	 {
-	    int num = Integer.parseInt(args[0]);
+		int num = Integer.parseInt(args[0]);
 		System.out.println("Factorial of " + num + " is " + factorial (num));
 	 }
 	 else
